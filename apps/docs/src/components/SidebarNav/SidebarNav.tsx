8ff4c150778aee095918d01b721a0fb6e520b5ac
Aditya Patel <aditya51696@gmail.com>
2026-07-25 15:05:22 -0400

    badge update to sidebar

--- apps/docs/src/components/SidebarNav/SidebarNav.tsx
+++ apps/docs/src/components/SidebarNav/SidebarNav.tsx
@@ -125,7 +125,7 @@ export default function SidebarNav() {
           <NavLink href="/field-notes">
             <span style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
             Field Notes
-            <Badge label="v1.6.3" variant="info" size="sm" />
+            <Badge label="v1.7.0" variant="info" size="sm" />
             </span>
           </NavLink>
           <NavLink href="/resources">Resources</NavLink>
